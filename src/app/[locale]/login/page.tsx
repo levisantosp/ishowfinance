4f17678c3087c21b111dd543b204afab9cd954e6
luizinmonster <luizgustavoidepedente2@gmail.com>
2025-09-20 21:34:54 -0300

    login e registro

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -4,7 +4,23 @@ export default async function Login() {
   const t = await getTranslations()
   return (
     <>
-      {t("hello")}
+      <div>
+        <h1>Entrar</h1>
+        <form>
+          <div>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Senha</label>
+            <input type="password" />
+          </div>
+          <button>Entrar</button>
+        </form>
+        <div>
+          <a>Ainda não tem uma conta? Registre-se</a>
+        </div>
+      </div>
     </>
   )
 }
