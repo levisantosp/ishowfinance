f98a0924e5d48ca4ffbee704a84f2d60a95893e7
levisantosp <contatowizard01@gmail.com>
2025-09-22 10:53:22 -0300

    Minor changes

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,16 @@
 declare global {
-  namespace NodeJs {
+  namespace NodeJS {
     interface ProcessEnv {
       POSTGRES_URI: string
+      GOOGLE_CLIENT_ID: string
+      GOOGLE_CLIENT_SECRET: string
+      MICROSOFT_CLIENT_ID: string
+      MICROSOFT_CLIENT_SECRET: string
+      BASE_URL: string
+      AUTH: string
+      NEXT_PUBLIC_AUTH: string
     }
   }
 }
+
+export {}
