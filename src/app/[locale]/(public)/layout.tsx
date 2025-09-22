7d7bb5196ae0edfadb56da3b3fda0741e9b1e56f
levisantosp <contatowizard01@gmail.com>
2025-09-22 16:26:29 -0300

    create a layout for public routes

---
+++ src/app/[locale]/(public)/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "../globals.css"
+import { hasLocale, NextIntlClientProvider } from "next-intl"
+import { notFound } from "next/navigation"
+import { getMessages, getTranslations, setRequestLocale } from "next-intl/server"
+import { routing } from "../../../i18n/routing.ts"
+import Header from "../../components/public/Header.tsx"
+import SessionChecker from "../../components/global/SessionChecker.tsx"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const generateMetadata = async(): Promise<Metadata> => {
+  const t = await getTranslations()
+  return {
+    title: t("metadata.title"),
+    description: t("metadata.description")
+  }
+}
+
+type Props = {
+  children?: React.ReactNode,
+  params: Promise<{ locale: string }>
+}
+
+export default async function RootLayout({ children, params }: Props) {
+  const { locale } = await params
+
+  if(!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  const messages = await getMessages()
+
+  return (
+    <html lang={locale}>
+      <body className={`${inter.className} min-h-screen flex flex-col`}>
+        <SessionChecker />
+        <Header />
+        <NextIntlClientProvider
+          messages={messages}
+          locale={locale}
+        >
+          <main className="flex-grow">
+            {children}
+          </main>
+        </NextIntlClientProvider>
+        {/* <Footer /> */}
+      </body>
+    </html>
+  )
+}
