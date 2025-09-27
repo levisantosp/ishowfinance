5835282b280193a7095d11cb712bfeabf60aa520
levisantosp <contatowizard01@gmail.com>
2025-09-27 14:37:58 -0300

    fix: ` Warning: Next.js inferred your workspace root, but it may not be correct.`

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com"
       }
     ]
+  },
+  turbopack: {
+    root: __dirname
   }
 }
 const withNextIntl = createNextIntlPlugin()
