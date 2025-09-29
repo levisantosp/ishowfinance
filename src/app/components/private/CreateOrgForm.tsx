4615f20dba2fee50184edff9e75bcb3e29387711
levisantosp <contatowizard01@gmail.com>
2025-09-29 16:06:07 -0300

    minor changes

--- src/app/components/private/CreateOrgForm.tsx
+++ src/app/components/private/CreateOrgForm.tsx
@@ -66,7 +66,7 @@ export default function CreateOrgForm() {
         />
 
         <input
-          type="text"
+          type="email"
           placeholder={t("pages.org.create.email")}
           className="w-full rounded-2xl border border-gray-500 pl-4 py-2"
           name="email"
