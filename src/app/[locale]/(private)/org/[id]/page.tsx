5a0624d66aeec2683267ef95a04387e9d352889f
levisantosp <contatowizard01@gmail.com>
2025-09-29 16:00:39 -0300

    add a page to redirect to `/org/[id]/overview`

---
+++ src/app/[locale]/(private)/org/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation"
+
+type Props = {
+  params: Promise<{
+    locale: string
+    id: string
+  }>
+}
+
+export default async function OrgHome(props: Props) {
+  const { locale, id } = await props.params
+
+  redirect(`/${locale}/org/${id}/overview`)
+}
