f13a249223519826f813b58a344b19c619998423
levisantosp <contatowizard01@gmail.com>
2025-10-01 17:57:46 -0300

    move the user profile to dashboard

--- src/app/[locale]/(private)/dash/page.tsx
+++ src/app/[locale]/(private)/dash/page.tsx
@@ -1,7 +1,30 @@
 import { getTranslations } from 'next-intl/server'
+import Org from '@components/private/org/Org'
+import Image from 'next/image'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { authClient } from '@/lib/auth-client'
+import { redirect } from 'next/navigation'
 
 export default async function Login() {
   const t = await getTranslations()
+
+  const session = await auth.api.getSession({ headers: await headers() })
+
+  if(!session) {
+    authClient.signOut({
+      fetchOptions: {
+        headers: {
+          auth: process.env.AUTH
+        },
+        onSuccess() {
+          redirect('/login')
+        }
+      }
+    })
+
+    return null
+  }
 
   return (
     <>
@@ -11,6 +34,52 @@ export default async function Login() {
         >
           {t('pages.dash.title')}
         </h1>
+      </div>
+
+      <div
+        className='flex justify-center'
+      >
+        <div
+          className='border border-gray-500 rounded-2xl p-10'
+        >
+          <div
+            className='flex items-center gap-5'
+          >
+            <Image
+              src={session.user.image as string}
+              height={100}
+              width={100}
+              alt='user profile icon'
+              className='rounded-full border-2 border-[#eed492]'
+            />
+
+            <div
+              className='flex flex-col'
+            >
+              <span
+                className='font-medium text-xl'
+              >
+                {session.user.name}
+              </span>
+
+              <span
+                className='text-gray-400 font-medium'
+              >
+                {t('pages.dash.profile.welcome')}
+              </span>
+            </div>
+          </div>
+
+          <h2
+            className='font-semibold text-2xl pt-10 text-center'
+          >
+            {t('pages.dash.profile.organizations')}
+          </h2>
+
+          <Org
+            userId={session.user.id}
+          />
+        </div>
       </div>
     </>
   )
