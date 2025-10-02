0fe6274aac54f4529996f5ca9d7f939dae5d1c34
levisantosp <contatowizard01@gmail.com>
2025-10-01 21:03:50 -0300

    use custom import

--- src/lib/middlewares/intl.ts
+++ src/lib/middlewares/intl.ts
@@ -1,4 +1,4 @@
 import createMiddleware from 'next-intl/middleware'
-import { routing } from '../../i18n/routing.ts'
+import { routing } from '@i18n/routing'
 
 export default createMiddleware(routing)
