f99a76a85942a2b0b48638722c582b17f927ed1f
levisantosp <contatowizard01@gmail.com>
2025-10-11 23:04:55 -0300

    feat: add orgs' total balance only for onwer or admin role

--- src/app/components/private/org/Org.tsx
+++ src/app/components/private/org/Org.tsx
@@ -23,13 +23,16 @@ type Org = {
   userId: string
   name: string
   members: Member[]
+  balance: string
+  currency: $Enums.Currency
 }
 
 type Props = {
   userId: string
+  locale: string
 }
 
-export default function Org({ userId }: Props) {
+export default function Org({ userId, locale }: Props) {
   const t = useTranslations()
 
   const [organizations, setOrganizations] = useState<Org[] | null>([])
@@ -43,14 +46,10 @@ export default function Org({ userId }: Props) {
           auth: process.env.NEXT_PUBLIC_AUTH,
           find: 'many',
           queryOptions: JSON.stringify({
-            where: { userId },
             include: {
               members: {
                 where: { userId }
               }
-            },
-            omit: {
-              balance: true
             }
           })
         }
@@ -85,23 +84,46 @@ export default function Org({ userId }: Props) {
         {organizations && organizations.length > 0 && organizations.map(org => (
           <div
             key={org.id}
-            className='border border-gray-500 py-3 px-7 rounded-2xl w-74 min-h-14'
+            className='border border-gray-500 py-3 px-7 rounded-2xl w-84 min-h-14 flex flex-col gap-2'
           >
-            <Link
-              href={`/org/${org.id}/overview`}
+            <div
+              className='flex justify-between items-center'
             >
-              <h3
-                className='text-xl font-semibold text-blue-400 underline'
+              <Link
+                href={`/org/${org.id}/overview`}
               >
-                {org.name}
-              </h3>
-            </Link>
+                <h3
+                  className='text-xl font-semibold text-blue-400 underline'
+                >
+                  {org.name}
+                </h3>
+              </Link>
 
-            <span
-              className='text-gray-400 font-medium'
-            >
-              {t(`pages.dash.profile.role.${org.members[0].role}`)}
-            </span>
+              <span
+                className='text-gray-400 font-medium'
+              >
+                {t(`pages.dash.profile.role.${org.members[0].role}`)}
+              </span>
+            </div>
+
+            {['OWNER', 'ADMIN'].includes(org.members[0].role) && (
+              <div>
+                <h3
+                  className='font-medium'
+                >
+                  {t('pages.dash.profile.total_balance')}
+                </h3>
+
+                <span
+                  className='text-gray-400'
+                >
+                  {(Number(org.balance) / 100).toLocaleString(locale, {
+                    currency: org.currency,
+                    style: 'currency'
+                  })}
+                </span>
+              </div>
+            )}
           </div>
         ))}
       </div>
