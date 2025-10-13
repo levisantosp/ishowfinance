fa792f8911c321b395c8db078fb78b26c4d78a0e
levisantosp <contatowizard01@gmail.com>
2025-10-13 00:10:06 -0300

    disable `@typescript-eslint/no-empty-object-type` rule

--- eslint.config.js
+++ eslint.config.js
@@ -60,7 +60,8 @@ export default defineConfig([
       'no-empty': 'off',
       'import/no-anonymous-default-export': 'off',
       'react-hooks/exhaustive-deps': 'off',
-      'react/react-in-jsx-scope': 'off'
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off'
     }
   }
 ])
