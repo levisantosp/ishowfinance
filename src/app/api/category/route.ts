df31fa679b8674cfddf6b93d675aa1b4c816b042
NauakXs <kauanmoreiradonascimento10862@gmail.com>
2025-10-13 15:34:31 -0300

    CRUD criaçao de categoria

---
+++ src/app/api/category/route.ts
@@ -0,0 +1,47 @@
+import { getSessionCookie } from 'better-auth/cookies'
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import { auth } from '@/lib/auth'
+
+export const POST = async (req: NextRequest) => {
+  const sessionCookie = getSessionCookie(req)
+
+  if (!sessionCookie) {
+    return NextResponse.json(
+      {
+        error: 'You must to be logged in'
+      },
+      {
+        status: 401
+      }
+    )
+  }
+
+  
+  const session = await auth.api.getSession({ headers: req.headers })
+
+  if(!session) {
+    return NextResponse.json(
+      {
+        error: 'You must to be logged in'
+      },
+      {
+        status: 401
+      }
+    )
+  }
+
+  const data: {
+    name: string
+    id: string
+  } = await req.json()
+
+  const category = await prisma.category.create({
+    data:{
+      name: data.name,
+      organizationId: data.id
+    }
+  })
+  
+  return NextResponse.json({ ok: true })
+}
