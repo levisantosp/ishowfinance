dd77081a10a94a768c16aea1268e66e195347292
levisantosp <contatowizard01@gmail.com>
2025-10-13 18:13:54 -0300

    feat: add create categories route

---
+++ src/app/[locale]/(private)/org/[id]/categories/create/page.tsx
@@ -0,0 +1,35 @@
+import { auth } from '@/lib/auth'
+import { prisma } from '@/lib/prisma'
+import { headers } from 'next/headers'
+import { notFound } from 'next/navigation'
+import CreateCategoryForm from '@components/private/org/CreateCategoryForm'
+
+type Props = {
+  params: Promise<{ id: string }>
+}
+
+export default async function Categories(props: Props) {
+  const { id } = await props.params
+
+  const session = await auth.api.getSession({ headers: await headers() })
+
+  const member = await prisma.member.findFirst({
+    where: {
+      userId: session?.user.id,
+      organizationId: id
+    },
+    include: {
+      organization: true
+    }
+  })
+
+  if(!member || !['OWNER', 'ADMIN'].includes(member.role)) {
+    notFound()
+  }
+
+  return (
+    <>
+      <CreateCategoryForm />
+    </>
+  )
+}
