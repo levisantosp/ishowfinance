f1c313db3a3f44258a329ad13a5acf6842903565
levisantosp <contatowizard01@gmail.com>
2025-10-13 00:11:16 -0300

    create `/org/[id]/edit` page

---
+++ src/app/[locale]/(private)/org/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { prisma } from '@/lib/prisma'
+import { notFound } from 'next/navigation'
+import { EditOrg } from '@components/private/org/EditOrg'
+
+type Props = {
+  params: Promise<{
+    id: string
+    locale: string
+  }>
+}
+
+export default async function Edit(props: Props) {
+  const { id } = await props.params
+
+  const session = await auth.api.getSession({ headers: await headers() })
+
+  const member = await prisma.member.findFirst({
+    where: {
+      userId: session?.user.id,
+      organizationId: id
+    }
+  })
+
+  if(!member || !['OWNER', 'ADMIN'].includes(member.role)) {
+    notFound()
+  }
+
+  return (
+    <>
+      <EditOrg id={id} />
+    </>
+  )
+}
