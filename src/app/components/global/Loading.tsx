b4f543e2362ec86a6cec4e19e70cf94815780791
levisantosp <contatowizard01@gmail.com>
2025-10-13 22:56:16 -0300

    fix: remove padding & change border size

--- src/app/components/global/Loading.tsx
+++ src/app/components/global/Loading.tsx
@@ -6,13 +6,13 @@ type Props = {
 export default function Loading(props: Props) {
   return (
     <div
-      className='flex justify-center p-10'
+      className='flex justify-center'
     >
       <div
-        className='flex items-center rounded-2xl p-5 gap-2'
+        className='flex items-center rounded-2xl gap-2'
       >
         <div
-          className={`w-${props.width} h-${props.height} border-4 border-gray-100 border-t-blue-500 rounded-full animate-spin`}
+          className={`w-${props.width} h-${props.height} border-3 border-gray-100 border-t-blue-500 rounded-full animate-spin`}
         />
       </div>
     </div>
