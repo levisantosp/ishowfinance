b66c4361756e2bf584f7110df2848585141d9ff3
levisantosp <contatowizard01@gmail.com>
2025-10-26 21:24:07 -0300

    fix(eslint): indent
       5:1  error  Expected indentation of 2 spaces but found 4   indent
      10:1  error  Expected indentation of 2 spaces but found 4   indent
      11:1  error  Expected indentation of 4 spaces but found 8   indent
      12:1  error  Expected indentation of 6 spaces but found 12  indent
      13:1  error  Expected indentation of 6 spaces but found 12  indent
      14:1  error  Expected indentation of 4 spaces but found 8   indent
      15:1  error  Expected indentation of 2 spaces but found 4   indent
      17:1  error  Expected indentation of 2 spaces but found 4   indent

--- src/app/api/member/route.ts
+++ src/app/api/member/route.ts
@@ -2,18 +2,17 @@ import { NextResponse, type NextRequest } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
 export const DELETE = async(req: NextRequest) => {
-    const data: {
-        organization: string
-        user: string
-    } = await req.json()
+  const data: {
+    organization: string
+    user: string
+  } = await req.json()
 
-    await prisma.member.deleteMany({
-        where: {
-            organizationId: data.organization,
-            userId: data.user
-        }
-    })
+  await prisma.member.deleteMany({
+    where: {
+      organizationId: data.organization,
+      userId: data.user
+    }
+  })
 
-    return NextResponse.json({ ok: true })
-
+  return NextResponse.json({ ok: true })
 }
