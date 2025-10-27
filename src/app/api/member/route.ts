27de03916b6f94b0cf777557a44c853b01bacb1d
NauakXs <kauanmoreiradonascimento10862@gmail.com>
2025-10-26 21:08:12 -0300

    Implementaçao da rota de deletar membros

--- src/app/api/member/route.ts
+++ src/app/api/member/route.ts
@@ -1,4 +1,19 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
-export const DELETE = async(req: NextRequest) => {}
+export const DELETE = async (req: NextRequest) => {
+    const data: {
+        organization: string
+        user: string
+    } = await req.json()
+
+    await prisma.member.deleteMany({
+        where: {
+            organizationId: data.organization,
+            userId: data.user
+        }
+    })
+
+    return NextResponse.json({ ok: true })
+
+}
