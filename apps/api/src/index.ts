b4dd47b73164cc8b6955b8b15d9b132bd01db2e9
levisantosp <contatowizard01@gmail.com>
2025-11-28 02:34:32 -0300

    feat(api): integrate openapi plugin for automatic documentation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from 'elysia'
 import { createTransaction } from './routes/transaction/create-transaction.ts'
+import { openapi } from '@elysiajs/openapi'
 
 new Elysia()
   .get('/', { message: 'Hello, world!' })
+  .use(openapi())
   .use(createTransaction)
   .listen(3001)
 
